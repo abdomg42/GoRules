e1cebf861c27fcaf1e637376e8535835fd45b594
abdomg42 <sonysam3a@gmail.com>
2026-07-15 14:26:46 +0100

    code for parsing pdf

--- RAG/indexing/vector_store.py
+++ RAG/indexing/vector_store.py
@@ -1,1 +1,20 @@
 
+from pypdf import PdfReader
+from pathlib import Path
+
+class DocumentParsing:
+    """for now only pdf files , next step is to add docs and other formats"""
+    def parse(self, file_path):
+        path = Path(file_path)
+        suffix = path.suffix.lower()
+        if suffix == ".pdf":
+            return self._parse_pdf(file_path)
+    
+    def _parse_pdf(self, file_path):
+        reader = PdfReader(file_path)
+        text=""
+        for page in reader.pages:
+            text += page.extract_text()
+        return text
+
+
