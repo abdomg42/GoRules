bf02a5d9c4320afc31f08413b92f60f3d7b28a57
abdomg42 <sonysam3a@gmail.com>
2026-07-17 12:18:07 +0100

    add ingesting docs with argparse lib

---
+++ scripts/ingest_doc.py
@@ -0,0 +1,27 @@
+import argparse
+from pathlib import Path 
+
+from RAG.ingestion.parser import DocumentParsing
+from RAG.ingestion.chunking import Chunk_section
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file",required=True)
+    parser.add_argument('--project', required=True)
+    args = parser.parse_args()
+
+    document_id = Path(args.file).stem
+
+    print(f"---start parsing {args.file}---")
+    sections = DocumentParsing().parse(args.file)
+    print(f"--- {len(sections)} sections extraites")
+
+    print("--- Chunking")
+    
+    chunks = Chunk_section().chunk_document(sections,document_id=document_id, project_id=args.project)
+    print(f"--->{len(chunks)} chunks generes ")
+
+    print("--- embeddings")
+    # à faire 
--- test.py
+++ test.py
@@ -5,4 +5,5 @@ from RAG.ingestion.chunking import Chunk_section
 # out = parser.parse("test.pdf")
 chunker = Chunk_section()
 chunks = chunker.chunk_document(file_path="test.pdf")
-print(chunks)
+print(chunks[-1])
+
