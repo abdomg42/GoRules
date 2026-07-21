4c0f24a9068aee0da82c8fe4ba227b548fff1813
abdomg42 <sonysam3a@gmail.com>
2026-07-21 09:55:59 +0100

    instatiate ingestion classes

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from src.ingestion.parsing import 
+from src.ingestion.parsing import Parser
 from src.ingestion.embedding import EmbeddingClient, EmbeddingAgent
-from src.ingestion.chunking import Chunk 
+from src.ingestion.chunking import Chunker
 from src.store import OllamaEmbeddingFunction
 
 app = FastAPI()
 
-_parser = 
+_parser = Parser()
+_chunker = Chunker()
+_embedding = EmbeddingAgent(embedding_client=EmbeddingClient())
