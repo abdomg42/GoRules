389914fcfd21bd267c667be54f70b8e7a57b57fb
abdomg42 <sonysam3a@gmail.com>
2026-08-04 14:53:07 +0100

    clean projet

--- src/agents/ocr.py
+++ src/agents/ocr.py
@@ -4,12 +4,14 @@ import fitz
 from PIL import Image
 import pytesseract
 import io 
+from base import LLMBase
 
-class OcrAgent(LLMAgent): 
+class OcrAgent(LLMBase): 
     def __init__(self,pages):
         self.pages = pages
     def pages_needs_ocr(self,page):
         text = page.get_text().strip()
         has_image = len(page.get_images(full=True)) > 0
         return len(text) < 20 and has_image
+        
 
--- test.py
+++
@@ -1,9 +0,0 @@
-from ingestion.parsing import DocumentParsing 
-from ingestion.chunking import Chunk_section
-
-# parser = DocumentParsing()
-# out = parser.parse("test.pdf")
-chunker = Chunk_section()
-chunks = chunker.chunk_document(file_path="test.pdf")
-print(chunks[-1])
-
